0fadff29a935cb79cc9991614b9eeaa59c943c50
anuj taywade <anujtaywade456@gmail.com>
2025-09-15 20:54:14 +0530

    authmiddleware

--- Backend/src/controller/authcontroller.js
+++ Backend/src/controller/authcontroller.js
@@ -36,7 +36,7 @@ exports.signup = async (req,res) => {
 exports.login = async (req,res) => {
     try {
         const {email,password} = req.body;
-        console.log(req.body)
+       
     if (!email || !password){
         return res.status(201).json({message : "above fields are required"})
     }
