9e95497502dabd8769d9d6d56e5a9e62a6d5d1a9
anuj taywade <anujtaywade456@gmail.com>
2025-09-23 14:47:17 +0530

    conditional rendering

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+  const location = useLocation()
+  const absoluteLoation = ["/","/signup","/login"]
+  const isAbsolute = absoluteLoation.includes(location.pathname)
+
   return (
     <div>
-    <div>
-         <h1 className='font-[poppins] font-bold text-5xl absolute bg-transparent left-15 top-5 '>
+    <div className={`${!isAbsolute? "border-b-2" : null}`}>
+         <h1 className={`font-[poppins] font-bold text-5xl bg-transparent ${isAbsolute ? "absolute left-15 top-5 ": "m-2 "} `}>
           Zoroa
          </h1>
     </div>
