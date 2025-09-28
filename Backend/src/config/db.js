c37a8c86d558c0cf38c49d0d4e76066477285357
anuj taywade <anujtaywade456@gmail.com>
2025-09-28 19:02:45 +0530

    removed old user from DB

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose")
 
 
 
-
 const connectDB = async () => {
     try{
         const conn = mongoose.connect(process.env.MONGO_URL,{
             useNewUrlPraser : true,
             useUnifiedTopology :true
         })
+
+
         console.log(`MongoDb connected ${(await conn).connection.host}`)
     }catch(error){
         console.log(`Error : ${error.message}`)
