6be84c289f8eb5a4eb07326c735659698256c7b1
anuj taywade <anujtaywade456@gmail.com>
2025-10-05 18:24:57 +0530

    socket.io in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const cookiePraser = require("cookie-parser")
+const http = require('http')
+const {Server} = require('socket.io')
+
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server,{
+  cors:{
+    origin:"http://localhost:5173",
+    methods:["GET","POST"]
+  }
+})
+
+io.on("connection",(socket)=>{
+  console.log("a user connected",socket.id)
+
+  socket.on("sendMessage",(data)=>{
+  console.log("mesage recieved : ",data)
+
+
+  socket.broadcast.emit ("receiver message : ",data)
+
+  socket.on("disconnect",(socket)=>{
+  console.log("a user disconnect",socket.id)
+
+})
+})
+})
+
+
+
+
+
 dotenv.config()
 
 app.use(cors({
@@ -36,6 +68,6 @@ app.get('/', (req, res) => {
 })
 
 const port = 7000
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`running on http://localhost:${port} for ${__dirname}`)
 })
