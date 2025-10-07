c223b08c4b758caef048f76c61ee79d6f8a00797
anuj taywade <anujtaywade456@gmail.com>
2025-10-07 16:09:28 +0530

    socket functionality

--- Frontend/src/components/MessagePanel.jsx
+++ Frontend/src/components/MessagePanel.jsx
@@ -1,8 +1,51 @@
 import React, { useContext } from 'react'
 import { AuthContext } from "../context/AuthContext";
+import { useSocket } from "../context/socketContext";
+
 
 const Message = ({ conversation , Theme, setTheme}) => {
+
+  const socket = useSocket()
   const { User } = useContext(AuthContext)
+
+  const [Messages, setMessages] = useState([]);
+  const [Message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (!socket) return;
+
+    if(conversation?._id){
+      socket.emit("join room",conversation?._id)
+    }
+
+    socket.on("recieve message",(data)=>{
+      if(data.conversationId === conversation?._id){
+        setMessages((prev)=>[...prev,data])
+      }
+    })
+
+    return()=>{
+    socket.off("recieve message")
+  }
+  }, [socket,conversation]);
+
+  const handleSend=()=>{
+    if(!Message.trim || !socket) return;
+
+    const newMesage = {
+      conversation: conversation._id,
+      sender : User.id,
+      text : Message,
+      createdAt : new Date()
+    }
+
+    socket.emit("send message",newMesage)
+    setMessage((prev)=>[...prev,newMesage])
+    setMessage("")
+  }
+
+
+  
 
   return (
     <div className="flex flex-col h-screen w-full pt-[80px]">
@@ -13,8 +56,19 @@ const Message = ({ conversation , Theme, setTheme}) => {
 
   
       <div className={`flex-1 overflow-y-auto p-4 ${Theme? "bg-black" : "bg-gray-50"} `}>
-        <div className="mb-2 w-max p-2 rounded"></div>
-        <div className="mb-2  w-max p-2 rounded ml-auto"></div>
+
+
+        <div className=""></div>
+        <div className=""></div>
+
+        {Message.map((msg,index)=>(
+          <div key={index} 
+          className={`mb-2 w-max p-2 rounded ${msg.sender=== User.id ? "mb-2  w-max p-2 rounded ml-auto bg-blue-500" : "bg-gray-300"}`}>
+              {msg.text}
+          </div>
+
+
+        ))}
       </div>
 
     
@@ -22,10 +76,14 @@ const Message = ({ conversation , Theme, setTheme}) => {
         <input
           type="text"
           name="message"
+          value={Message}
+          onChange={(e)=>setMessage(e.target.value)}
           placeholder="Start typing..."
           className="flex-1 h-12 px-4 border rounded-lg focus:outline-none"
         />
-        <button className="ml-2 px-4 bg-blue-500 text-white rounded-lg">Send</button>
+        <button
+        onClick={handleSend}
+        className="ml-2 px-4 bg-blue-500 text-white rounded-lg">Send</button>
       </div>
     </div>
   )
