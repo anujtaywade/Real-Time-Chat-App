8b4795c2483c1c91204f6091df07cb1ef5ec99ed
anuj taywade <anujtaywade456@gmail.com>
2025-10-22 17:41:41 +0530

    fix cookie issue v0.1

--- Backend/src/controller/authcontroller.js
+++ Backend/src/controller/authcontroller.js
@@ -69,7 +69,7 @@ exports.login = async (req,res) => {
         res.cookie("token",token,{
             httpOnly : true,
             secure : process.env.NODE_ENV === "production",
-            sameSite : "lax",
+            sameSite : "none",
             maxAge : 7 * 24 * 60 * 60 * 1000
         })
         res.status(200).json({message : "login successful",
@@ -96,7 +96,7 @@ exports.logout=async (req,res) => {
         res.clearCookie("token",{
             httpOnly : true,
             secure : process.env.NODE_ENV === "production",
-            sameSite : "lax",
+            sameSite : "none",
         })
         res.status(200).json({message:"logout successful"})
 
