20f1b49290cc40dde01a1d41492de62dbd16a802
anuj taywade <anujtaywade456@gmail.com>
2025-12-22 00:53:43 +0530

    ui changes

--- Backend/src/routes/addFriendsRouth.js
+++ Backend/src/routes/addFriendsRouth.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const {addFriend} = require('../controller/addFriend')
 const auth = require("../middleware/authMiddleware")
 
-router.post("/add-friend/:id", auth ,addFriend)
+router.post("/add-friend", auth ,addFriend)
 
 module.exports = router
